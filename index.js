4add0a87c9764541512489838c67d0a6f75ebe1c
Chris Witte <christopher.t.witte@gmail.com>
2022-08-10 12:03:14 -0400

    Added array filter functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (drivers, str) {
+    return drivers.filter(match => match.toLowerCase() === str.toLowerCase())
+}
+
+findMatching(drivers, "bobby");
+
+function fuzzyMatch (drivers, str) {
+    return drivers.filter(letters => letters.indexOf(str) === 0)
+}
+
+fuzzyMatch(drivers, "Sa")
+
+function matchName (drivers, str) {
+    return drivers.filter(names => names.name === str)
+}
